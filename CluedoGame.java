1cf71a096c69ae44505021d79e73ac5d14c9c381
lil-izzy-vert <isabellaketley@hotmail.com>
2020-07-28 00:40:54 +1200

    Delete CluedoGame.java

--- CluedoGame.java
+++
@@ -1,9 +0,0 @@
-public class CluedoGame {
-    private int minPlayers;
-    private int maxPlayers;
-    private int numPlayers;
-    private java.util.ArrayList<Card> cards;
-    private Suggestion murder;
-    private java.util.ArrayList<Player> players;
-    private Board board;
-}
